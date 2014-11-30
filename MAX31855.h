e65400ab42e27e9418389a474168fbf782a2e91a
Joe <jowood4@gmail.com>
2014-11-29 19:57:22 -0500

    First commit for MAX31855

---
+++ MAX31855.h
@@ -0,0 +1,30 @@
+#ifndef MAX31855_H
+#define MAX31855_H
+
+#if ARDUINO >= 100
+ #include "Arduino.h"
+#else
+ #include "WProgram.h"
+#endif
+
+class MAX31855
+{
+public:
+	MAX31855(void);
+	void setup(uint8_t data, uint8_t clock, uint8_t latch);
+	uint8_t spi_shift(void);
+	void read_temp(void);
+
+        uint8_t thermocouple_temp;
+        uint8_t fault;
+        uint8_t chip_temp;
+        uint8_t scv_fault;
+        uint8_t scg_fault;
+        uint8_t oc_fault;
+private:
+	uint8_t _data;
+	uint8_t _clock;
+	uint8_t _latch;
+
+};
+#endif
