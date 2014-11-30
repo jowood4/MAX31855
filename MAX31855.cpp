a6a07b5518e7d9ceb21f9e97e1a3eb54e09c22e5
Joe <jowood4@gmail.com>
2014-11-30 14:07:45 -0500

    Added Arduino SPI control

--- MAX31855.cpp
+++ MAX31855.cpp
@@ -1,8 +1,31 @@
 #include "MAX31855.h"
 
+
 MAX31855::MAX31855(void)
 {
 }
+
+#if defined(SPI_transfer)
+
+void MAX31855::setup(uint8_t latch)
+{
+	_latch = latch;
+	
+	pinMode(_latch,1);
+
+	digitalWrite(_latch,1);
+
+	SPI.begin();
+}
+
+uint8_t MAX31855::spi_shift(void)
+{
+	uint8_t temp_data = 0;
+	temp_data = SPI.transfer(255);
+	return temp_data;
+}
+
+#else
 
 void MAX31855::setup(uint8_t data, uint8_t clock, uint8_t latch)
 {
@@ -29,22 +52,29 @@ uint8_t MAX31855::spi_shift(void)
 		digitalWrite(_clock,1);
 		digitalWrite(_clock,0);
 	}
+
 	return temp_data;
 }
+
+#endif
 
 void MAX31855::read_temp(void)
 {
 	uint8_t data[4];
 	uint8_t j;
 
+	digitalWrite(_latch,0);
 	for(j=0; j<4; j++)
 	{
 		data[j] = spi_shift();
+		//Serial.println(data[j]);
 	}
+	delay(1000);  
+	digitalWrite(_latch,1);
 
-        thermocouple_temp = (data[0]<<6) + (data[1]>>2);
+        thermocouple_temp = ((data[0]<<6) + (data[1]>>2))*0.25;
         fault = data[1] & 0x01;
-        chip_temp = (data[2]<<4) + (data[3]>>4);
+        chip_temp = ((data[2]<<4) + (data[3]>>4))*0.0625;
         scv_fault = data[3] & 0x04;
         scg_fault = data[3] & 0x02;
         oc_fault = data[3] & 0x01;
--- MAX31855.h
+++ MAX31855.h
@@ -7,17 +7,27 @@
  #include "WProgram.h"
 #endif
 
+#define SPI_transfer 1
+
+#if defined(SPI_transfer)
+#include "SPI.h"
+#endif
+
 class MAX31855
 {
 public:
 	MAX31855(void);
+	#if defined(SPI_transfer)
+	void setup(uint8_t latch);
+	#else
 	void setup(uint8_t data, uint8_t clock, uint8_t latch);
+	#endif
 	uint8_t spi_shift(void);
 	void read_temp(void);
 
-        uint8_t thermocouple_temp;
+        double thermocouple_temp;
         uint8_t fault;
-        uint8_t chip_temp;
+        double chip_temp;
         uint8_t scv_fault;
         uint8_t scg_fault;
         uint8_t oc_fault;
@@ -25,6 +35,5 @@ private:
 	uint8_t _data;
 	uint8_t _clock;
 	uint8_t _latch;
-
 };
 #endif
